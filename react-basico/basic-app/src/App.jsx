3e7dd7ec67ae0958e63c5ab3c98157f176eeda0a
Aleezitu <alee_meier@outlook.com>
2023-10-10 12:27:11 -0300

    Relese merge

--- react-basico/basic-app/src/App.jsx
+++ react-basico/basic-app/src/App.jsx
@@ -3,16 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AppViewer from './components/AppViewer'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-<>
- <h1>Meu primeiro WebApp</h1>
- <AppViewer />
-</>
-  )
+const App = () => {
+ return(
+  <>
+  <h1>Meu Primeiro WebApp</h1>
+  <AppViewer/>
+  </>
+ )
 }
-
 export default App
--- react-basico/basic-app/src/components/AppViewer.jsx
+++ react-basico/basic-app/src/components/AppViewer.jsx
@@ -1,15 +1,47 @@
-import styles from './AppViewer.module.css'
-//Camada de Compilação de Serviços
-function somaNumero(n1, n2){
-  return(n1+n2)
-}
+import styles from './AppViewer.module.css';
+
+// Camada de Compilação de Serviços
 const AppViewer = () => {
-  //Retorno no display
+  const professores = [
+    { id: 1, nome: "José Armando Vargas", idade: 42, formacao: "Engenharia da Computação", titulacao: "Lato Sensu" },
+    { id: 2, nome: "Ana Maria Silva", idade: 30, formacao: "Ciência da Computação", titulacao: "Mestrado" },
+    { id: 3, nome: "Carlos Oliveira", idade: 35, formacao: "Engenharia Elétrica", titulacao: "Doutorado" },
+    { id: 4, nome: "Maria Eduarda Santos", idade: 28, formacao: "Sistemas de Informação", titulacao: "Graduação" },
+    { id: 5, nome: "Pedro Alves", idade: 25, formacao: "Tecnologia da Informação", titulacao: "Pós-Graduação" },
+    { id: 6, nome: "Lúcia Pereira", idade: 38, formacao: "Engenharia de Software", titulacao: "Lato Sensu" },
+    { id: 7, nome: "Fernando Garcia", idade: 29, formacao: "Ciência da Computação", titulacao: "Doutorado" },
+    { id: 8, nome: "Rita Souza", idade: 31, formacao: "Engenharia Eletrônica", titulacao: "Mestrado" },
+    { id: 9, nome: "Gustavo Mendes", idade: 27, formacao: "Engenharia de Redes", titulacao: "Pós-Graduação" }
+  ];
+
   return (
     <>
-      <div className={somaNumero(2, 2) >10 ? console.log("Bunda"): console.log("Beiço")}>AppViewer</div>
+      <h2>Lista de professores</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Idade</th>
+            <th>Formação</th>
+            <th>Titulação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {professores.map((professor) => (
+            <tr className={professor.id % 2 == 1 ? styles.linhaCinza : ''} key={professor.id}>
+              <td>{professor.id}</td>
+              <td>{professor.nome}</td>
+              <td>{professor.idade}</td>
+              <td>{professor.formacao}</td>
+              <td className={professor.titulacao === "Mestrado" || professor.titulacao === "Doutorado" ? styles.superTitulo : styles.normalTitulo}>
+                {professor.titulacao}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
-  )
+  );
 }
-
-export default AppViewer
+export default AppViewer;
